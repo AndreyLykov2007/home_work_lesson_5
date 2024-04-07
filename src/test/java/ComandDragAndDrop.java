29a738bd16704a483022e18cf229ff6269b4422d
Andrey Lykov <lykov2007@gmail.com>
2024-04-07 15:45:53 +0100

    DragAndDrop exercise is done

--- src/test/java/ComandDragAndDrop.java
+++ src/test/java/ComandDragAndDrop.java
@@ -1,19 +1,18 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class DragAndDrop {
+public class ComandDragAndDrop {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://the-internet.herokuapp.com";
     }
-    @Test
-    void dragAndDrop(){
-    open("/drag_and_drop");
-    actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
-    $$(".column").first().shouldHave(text("B"));
+        @Test
+    void dragAndDropThroughCommand(){
+        open("/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
     }
-}
+    }
